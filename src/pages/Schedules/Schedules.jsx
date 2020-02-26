9b801fd1658eaf1227ab830aed7f7baf3a841a86
Galata Tona <tonagalata+2@gmail.com>
2020-02-26 18:19:42 -0500

    updates schedule module

--- src/pages/Schedules/Schedules.jsx
+++ src/pages/Schedules/Schedules.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Calendar from 'react-calendar'
+import styles from './Schedules.module.css'
 
-const Schedules = (props) => {
-  return ( 
-    <div>
-      <h1>
-        Schedules
-      </h1>
-    </div>
-   );
+class Schedules extends Component {
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
+  
+  render() { 
+    return ( 
+      <div>
+        <div className={styles.scheduleContainer}>
+          <div>
+            <Calendar
+              className={styles.reactCalendar}
+              onChange={this.onChange}
+              value={this.state.date}
+              />
+          </div>
+          <div className={styles.scheduleContent}>
+            <div className={styles.scheduleTitles}>
+              Schedules
+            </div>
+            <div className={styles.scheduleBox}>
+              Box
+            </div>
+          </div>
+        </div>
+      </div>
+     );
+  }
 }
  
 export default Schedules;
