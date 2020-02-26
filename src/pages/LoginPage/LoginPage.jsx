1469150f26de461799bf1de377a1f2198ef3f2ce
Galata Tona <tonagalata+2@gmail.com>
2020-02-26 18:07:42 -0500

    adds loginpage and basic styling

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginSignup = (props) => {
+  return ( 
+    <div>
+      <h1>
+        LoginSignup
+      </h1>
+    </div>
+   );
+}
+ 
+export default LoginSignup;
