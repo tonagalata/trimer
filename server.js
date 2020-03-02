68bdc789fbcba23ec70fba50e2e1fc44d0794524
Galata Tona <tonagalata+2@gmail.com>
2020-03-02 17:59:06 -0500

    adds server and database connection boilerplate

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const servefavicon = require('serve-favicon')
+const logger = require('morgan')
+
+const app = express();
+
+//configs
+require('dotenv').config()
+require('./config/database')
+
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(servefavicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+//Routes - serving up files
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api', require('./routes/api/hairprofessionals'))
+app.use('/api/hairprofessionals', require('./routes/api/reviews'))
+
+app.get('*/', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+
+
+//App Listening 
+const Port = process.env.PORT || 3001;
+
+app.listen(Port, () => {
+  console.log(`Express app listening on port ${Port}`)
+});
