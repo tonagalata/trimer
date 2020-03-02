e0a9796ddb1feed79b07c958406c20dc1eb1ccb7
Galata Tona <tonagalata+2@gmail.com>
2020-03-02 18:03:46 -0500

    adds token service to App

---
+++ src/Services/tokenService.js
@@ -0,0 +1,36 @@
+function removeToken(){
+  localStorage.removeItem('token')
+}
+
+function getToken() {
+  let token = localStorage.getItem('token');
+  if(token) {
+    const payload = JSON.parse(atob(token.split('.')[1]))
+    if(payload.exp < Date.now() / 1000) {
+      localStorage.removeItem('token');
+      token = null
+    }
+  }
+  return token
+}
+
+function getUserFromToken() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null
+  // We're going to get token with the user
+}
+
+function setToken(token) {
+  if(token) {
+    localStorage.setItem('token', token)
+  } else {
+    localStorage.removeItem('token')
+  }
+}
+
+
+export default {
+  setToken,
+  getUserFromToken,
+  removeToken
+}
