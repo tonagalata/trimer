81d0e5c3f72713d92268e06547aa2a736113d050
Galata Tona <tonagalata+2@gmail.com>
2020-03-05 08:56:12 -0500

    updates ViewSalon component and styling

--- src/pages/ViewSalon/ViewSalon.jsx
+++ src/pages/ViewSalon/ViewSalon.jsx
@@ -1,137 +1,127 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom'
-// import trimerService from '../../Services/trimerService'
-// import StarRatings from 'react-star-ratings'
-// import barbershop from '../../Images/barbershop.png'
-// import spa from '../../Images/spa.jpg'
-// import salon from '../../Images/salon.jpg'
+import trimerService from '../../Services/trimerService'
+import StarRatings from 'react-star-ratings'
+import barbershop from '../../Images/barbershop.png'
+import spa from '../../Images/spa.jpg'
+import salon from '../../Images/salon.jpg'
 import styles from './ViewSalon.module.css'
+import { Link } from 'react-router-dom';
 
 class ViewSalon extends Component {
-  state = this.getInitialState()
 
-  // getInitialState(){
-  //   return {
-  //    salon: []
-  //   }
-  // }
+  state = {
+    salon: this.getInitialState()
+  }
 
-  componentWillMount = async () => {
-    try {
-      const singleSalon = await this.props.salon
-    } catch (error) {
-      
+  getInitialState(){
+    return {
+     salon: []
     }
   }
 
-  // componentDidMount = async () => {
-  //  try {
-  //    const data = await trimerService.index()
-  //     console.log(data)
-  //      this.setState({
-  //       salons: data
-  //     })
-  //  } catch (error) {
-  //    console.log(error)
-  //  }
-  // }
+  
+  componentDidMount = async () => {
+    try {
+      const data = await trimerService.index()
+        this.setState({
+         salon: data
+       })
+    } catch (error) {
+      console.log(error)
+    }
+   }
+
 
   render() { 
     return ( 
       <section className='container'>
-      <h1 className={styles.main}>
-      Hair Salons
-      </h1>
+
+      {console.log(this.state.salon)}
       {
+        this.state.salon.length > 0 ?
         <div>
           {
-            this.props.match.params.id === this.props.salon
-            ?
-            console.log(this.props)
-            :
-            'No such Salon'
+          this.state.salon.map((d, idx)=> 
+            <>
+              {
+              d._id === this.props.match.params.id 
+              ? 
+              <>
+                <h1 className={styles.main}>
+                  {d.businessName}
+                </h1>
+                <div key={idx} className={styles.salonImage}>
+                { d.businessType === "Barbershop" ?
+                  <img src={barbershop} alt='barbershop'/>  
+                  : ''
+                }
+                { d.businessType === "Spa" ?
+                    <img src={spa} alt='Spa'/>  
+                  : ''
+                }
+                { d.businessType === "Salon" ?
+                  <img src={salon} alt='Salon'/>  
+                  : ''
+                }
+                </div>
+                <div className={styles.businessComponent}>
+                  <div className='chip' key={d.businessType}>{d.businessType}</div>
+                  <div key={d.address}>{d.address}</div>
+                  <Link to={`/salon/${d._id}/review`}><button className='btn btn-primary'>Add review</button></Link>
+                </div>
+                <div className={styles.reviewsContainer}>
+                { 
+                !d.reviews.length > 0
+                ?  
+                <div>
+                  <h3>Reviews</h3>
+                  <hr/>
+                      No Reviews Yet
+                </div>
+                :
+                <div>
+                <h3>Reviews</h3>
+                <hr/>
+                <div className={styles.reviewsComments}>
+                  {d.reviews.map( r => 
+                <div 
+                  key={r._id}
+                  className={styles.reviewsContent}
+                  > 
+                  <div>{r.content}</div>
+                  <StarRatings
+                  starDimension="1rem"
+                  starSpacing=".01rem"
+                  starRatedColor={r.rating > 3 ? 'green' : 'red'}
+                  rating={r.rating} />
+                  </div>
+                  )}
+                  </div>
+                  </div>
+                  }
+                </div>
+                <hr/>
+              </>
+              :
+              ''
+            }
+            </>
+            
+            )
           }
+
         </div>
+        : 'Loading...'
 
-      // <div className={styles.prosContainer}>
-      //   <div className={styles.stylestsContainer}>
-      // { this.state.salons.map((d, idx) => 
-      //     <div key={d._id} className={styles.salonsContainer}>
-      //         <h2 key={d.businessName}>{d.businessName}</h2>
-      //         <div key={idx} className={styles.salonImage}>
-      //           { d.businessType === "Barbershop" ?
-      //           <Link to={`salon/${d._id}`}>
-      //             <img src={barbershop} alt='barbershop'/>  
-      //           </Link>
-      //             : ''
-      //           }
-      //           { d.businessType === "Spa" ?
-      //             <Link to={`salon/${d._id}`}>
-      //               <img src={spa} alt='Spa'/>  
-      //             </Link>
-      //             : ''
-      //           }
-      //           { d.businessType === "Salon" ?
-      //           <Link to={`salon/${d._id}`}>
-      //             <img src={salon} alt='Salon'/>  
-      //           </Link>
-      //             : ''
-      //           }
-      //         </div>
-      //         <div key={d.businessType}>{d.businessType}</div>
-      //         <div key={d.address}>{d.address}</div>
-      //         {/* <div key={idx}>{d.addedBy && Object.values(d.addedBy)}</div>
-      //         {console.log(d.addedBy)}           */}
-      //         <Link to={`salon/${d._id}/review`}><button className='btn btn-primary'>Add review</button></Link>
-      //         <div className={styles.reviewsContainer}>
-      //           { 
-      //           !d.reviews.length > 0
-      //           ?  
-      //           <div>
-      //             <h3>Reviews:</h3>
-      //                 No Reviews Yet
-      //           </div>
-      //           :
-      //           <div>
-      //           <h3>Reviews:</h3>
-      //           <div className={styles.reviewsComments}>
-      //             {d.reviews.map( r => 
-      //           <div 
-      //             key={r._id}
-      //             className={styles.reviewsContent}
-      //             > 
-      //             <div>{r.content}</div>
-
-      //             <StarRatings
-      //             starDimension="1rem"
-      //             starSpacing=".01rem"
-      //             starRatedColor={r.rating > 3 ? 'green' : 'red'}
-      //             rating={r.rating} />
-      //             </div>
-      //             )}
-      //             </div>
-      //             </div>
-      //             }
-      //           </div>
-      //             <div>
-      //               {d.schedule.map(s => 
-      //                  <div key={s._id}> {s.scheduledBy} : {s.scheduledDate}
-
-      //                  {
-      //                   s.scheduledBy === d._id ? d.name : ' Not a user'
-      //                  }
-                       
-      //                  { console.log(d._id) }
-      //                  { console.log(this.state.salons) }
-      //                  </div>
-      //               )}
-      //             </div>
-      //       </div>
-      //           )}
-      //     </div>
-      //   </div>
       }
-    </section>
+      {
+        // this.state.salon.map((s, idx) =>
+        // <div key={idx}>{s._id}</div>
+        // )
+      }
+
+         
+      </section>
      );
   }
 }
