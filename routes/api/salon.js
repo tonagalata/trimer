b57435642796e6b979bddb82a780c345226835ba
Galata Tona <tonagalata+2@gmail.com>
2020-03-05 08:53:33 -0500

    adds auth and helper function to routes

--- routes/api/salon.js
+++ routes/api/salon.js
@@ -4,6 +4,17 @@ const salonCtlr = require('../../controllers/salon')
 
 router.get('/salon', salonCtlr.index)
 router.get('/featured', salonCtlr.getFeatured)
-router.post('/create-salon', salonCtlr.create)
+
+router.use(require('../../config/auth'))
+
+router.post('/create-salon', isAuthenticated,salonCtlr.create)
+
+/* Helper Function */
+
+function isAuthenticated(req, res, next) {
+  if(req.user) return next();
+  return res.status(401).json({ msg: 'Not Authorized'})
+}
+
 
 module.exports = router;
--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const scheduleCtlr = require('../../controllers/schedule')
 
 router.get('/:id/schedule', scheduleCtlr.index)
-router.post('/:id/schedule', scheduleCtlr.create)
+
+router.use(require('../../config/auth'))
+
+router.post('/:id/schedule', isAuthenticated, scheduleCtlr.create)
 // router.delete('/schedule/:id/', scheduleCtlr.deleteOneReview)
 
+
+/* Helper Function */
+
+function isAuthenticated(req, res, next) {
+  if(req.user) return next();
+  return res.status(401).json({ msg: 'Not Authorized'})
+}
 
 module.exports = router;
