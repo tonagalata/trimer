ed211584e242acd2080a66a43dbbce88af0ccc5c
Galata Tona <tonagalata+2@gmail.com>
2020-03-05 08:52:53 -0500

    adds auth and helper function to routes

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -2,9 +2,21 @@ const express = require('express')
 const router = express.Router();
 const reviewsCtlr = require('../../controllers/reviews')
 
+
 router.get('/:id/review', reviewsCtlr.index)
-router.post('/:id/review', reviewsCtlr.create)
-router.delete('/review/:id/', reviewsCtlr.deleteOneReview)
+
+router.use(require('../../config/auth'))
+
+router.post('/:id/review', isAuthenticated, reviewsCtlr.create)
+router.delete('/review/:id/', isAuthenticated, reviewsCtlr.deleteOneReview)
+
+
+/* Helper Function */
+
+function isAuthenticated(req, res, next) {
+  if(req.user) return next();
+  return res.status(401).json({ msg: 'Not Authorized'})
+}
 
 
 module.exports = router;
