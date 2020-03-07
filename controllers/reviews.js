b7e2c53417fc6a445b187bdb41232c413b9c1c6b
Galata Tona <tonagalata+2@gmail.com>
2020-03-07 10:53:44 -0500

    updates delete and update functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,7 +3,8 @@ const Salon = require('../models/salon')
 module.exports = {
   index,
   create,
-  deleteOneReview
+  deleteOneReview,
+  UpdateOneReview
 }
 
 async function index(req, res) {
@@ -32,8 +33,19 @@ async function create(req, res) {
 async function deleteOneReview(req, res) {
   try {
     const salon = await Salon.findById(req.params.id);
-    const reviews = await salon.reviews
-    reviews.splice(reviews._id, 1)
+    const reviews = await salon.reviews.splice(req.params.idx, 1)
+    salon.save({})
+    res.json(reviews);
+  } catch (error) {
+    console.log(error)
+     res.status(400).json(error);
+   }
+}
+
+async function UpdateOneReview(req, res) {
+  try {
+    const salon = await Salon.findById(req.params.id);
+    const reviews = await salon.reviews.splice(req.params.idx, 1, req.body)
     salon.save({})
     res.json(reviews);
   } catch (error) {
